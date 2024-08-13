fd8d7c251d1bff70b5a668592ec133f76edf97d7
K-KangH <daus1006@gmail.com>
2024-08-13 20:46:25 +0900

    20:45

--- front/src/components/sections/01_Video.js
+++ front/src/components/sections/01_Video.js
@@ -1,69 +1,3 @@
-// import React, { useEffect, useRef } from 'react';
-// import YouTube from 'react-youtube';
-// import { useInView } from 'react-intersection-observer';
-
-// function Video({ introDone }) {
-//     const { ref: inViewRef, inView: isVisible } = useInView({
-//         threshold: 0.6,
-//     });
-
-//     const playerRef = useRef(null);
-
-//     const onReady = (event) => {
-//         playerRef.current = event.target;
-//     };
-
-//     const opts = {
-//         height: '80%',
-//         width: '95%',
-//         playerVars: {
-//             autoplay: 0,
-//             rel: 0,
-//             controls: 0,
-//             disablekb: 1,
-//             fs: 0,
-//             loop: 1,
-//             playlist: 'WqNBIkFOoE4',
-//         },
-//     };
-//     useEffect(() => {
-//         if (introDone) {
-//             if (isVisible) {
-//                 // playerRef.current.mute();
-//                 playerRef.current.playVideo();
-//             } else {
-//                 // playerRef.current.mute();
-//                 playerRef.current.pauseVideo();
-//             }
-//         }
-//     }, [isVisible, introDone]);
-
-//     return (
-//         <>
-//             <section
-//                 id="video-container"
-//                 ref={inViewRef}
-//                 style={{ display: 'block' }}
-//             >
-//                 <YouTube
-//                     videoId="WqNBIkFOoE4"
-//                     style={{
-//                         display: 'flex',
-//                         alignItems: 'top',
-//                         justifyContent: 'center',
-//                         height: '100vh',
-//                         width: '100%',
-//                     }}
-//                     opts={opts}
-//                     onReady={onReady}
-//                     ref={playerRef}
-//                 />
-//             </section>
-//         </>
-//     );
-// }
-
-// export default Video;
 import React, { useEffect, useRef, useState } from 'react';
 import YouTube from 'react-youtube';
 import { useInView } from 'react-intersection-observer';
@@ -98,8 +32,10 @@ function Video() {
     useEffect(() => {
         if (playerReady && playerRef.current) {
             if (isVisible) {
+                playerRef.current.mute();
                 playerRef.current.playVideo();
             } else {
+                playerRef.current.mute();
                 playerRef.current.pauseVideo();
             }
         }
